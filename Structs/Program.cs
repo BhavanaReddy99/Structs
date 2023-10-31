e69be14e10f95ca57825a0165bc24b1a62411708
BhavanaReddy99 <jorrigalabhavana01@gmail.com>
2023-10-31 12:05:37 +0530

    Add project files.

---
+++ Structs/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+
+    struct Student
+    {
+        public string Name;
+        public int Age;
+        public string Course;
+        public double GPA;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doApp();
+        }
+
+        static void doApp()
+        {
+            Student student;
+
+            Console.Write("Enter student's name: ");
+            student.Name = Console.ReadLine();
+
+            Console.Write("Enter student's age: ");
+            if (int.TryParse(Console.ReadLine(), out student.Age) == false)
+            {
+                Console.WriteLine("Invalid input for age. Please enter a valid integer.");
+                return;
+            }
+
+            Console.Write("Enter student's course: ");
+            student.Course = Console.ReadLine();
+
+            Console.Write("Enter student's GPA: ");
+            if (double.TryParse(Console.ReadLine(), out student.GPA) == false)
+            {
+                Console.WriteLine("Invalid input for GPA. Please enter a valid number.");
+                return;
+            }
+
+            Console.WriteLine("\nStudent Details:");
+            Console.WriteLine($"Name: {student.Name}");
+            Console.WriteLine($"Age: {student.Age}");
+            Console.WriteLine($"Course: {student.Course}");
+            Console.WriteLine($"GPA: {student.GPA}");
+        }
+    }
+
+}
